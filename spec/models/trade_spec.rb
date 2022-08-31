7da5691d68ec2d5b32d254a5ca79da51e8a4485b
AlinaMustaqeem <alina.mustaqeem@devsinc.com>
2022-08-31 14:26:43 +0500

    test cases for trade

---
+++ spec/models/trade_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+RSpec.describe Trade, type: :model do
+  describe 'association' do
+    it { should belong_to(:sending_user).class_name(:User) }
+    it { should belong_to(:recieving_user).class_name(:User) }
+  end
+
+  describe 'enums' do
+    it { should define_enum_for(:request_status).with_values(rejected: 0, accepted: 1, pending:2)}
+  end
+
+end
